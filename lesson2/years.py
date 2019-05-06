9e55d10ac95326d425076d977e284726a576b9c3
Constantine Kormashev <constantine.kormashev@gmail.com>
2019-05-06 09:09:09 +0300

    fix years

--- lesson2/years.py
+++ lesson2/years.py
@@ -6,7 +6,6 @@ from game_scenario import PHRASES
 async def show_years(canvas, years):
 
     assert bool(years), AssertionError("Years has to be initiated with int value.")
-    assert years[0] >= 1957, AssertionError("Years has to be at least 1957 and more.")
 
     height, width = canvas.getmaxyx()
     row = height // 2
@@ -23,5 +22,15 @@ async def show_years(canvas, years):
         await sleep_for(15)
 
         draw_frame(canvas, row, frame_column, year_frame, negative=True)
+
+
+async def years_increment(years):
+
+    assert bool(years), AssertionError("Years has to be initiated with int value.")
+    assert years[0] >= 1957, AssertionError("Years has to be at least 1957 and more.")
+
+    while True:
+
+        await sleep_for(15)
 
         years[0] += 1
