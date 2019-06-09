f4687b291a539f16bbb1a51db5a9bd9659d57ef6
Constantine Kormashev <constantine.kormashev@gmail.com>
2019-06-09 08:50:55 +0300

    add separate logger

--- lesson4/chat_reader.py
+++ lesson4/chat_reader.py
@@ -5,10 +5,13 @@ from os import getenv
 
 from aiofile import AIOFile, Writer
 
-from chat_common import get_logged_message, chat_connector, get_argparser, _non_empty_printable, run_client, _port
+from chat_common import get_logged_message, chat_connector, get_argparser, _non_empty_printable, run_client, _port, setup_general_log
 
 
-async def client_reader(filename, chat_connector):
+reader_log = logging.getLogger("Chat reader")
+
+
+async def read_chat(filename, chat_connector):
 
     assert bool(filename) and filename.isprintable(), AssertionError("Filename has to be non-empty printable.")
 
@@ -30,7 +33,6 @@ async def read_write_lines(reader, _, writer):
     data = await reader.readline()
 
     while data:
-
         await writer(get_logged_message(data.decode("utf-8", "ignore")))
         data = await reader.readline()
 
@@ -44,23 +46,22 @@ def get_args():
                         default=int(getenv("CHAT_PORT", 5000)))
 
     parser.add_argument("-H", "--history", action="store", type=_non_empty_printable,
-                    help="messages history, default is ./messages.history",
-                    default=getenv("CHAT_HISTORY", "./messages.history"))
+                        help="messages history, default is ./messages.history",
+                        default=getenv("CHAT_HISTORY", "./messages.history"))
 
     return parser.parse_args()
 
 
 if __name__ == '__main__':
-
     options = get_args()
 
-    # logging settings
-    logging.basicConfig(filename=options.log, level=(options.loglevel * 10),
-                        format="[%(asctime)s] %(levelname)s / %(funcName)s / %(message)s",
-                        datefmt="%Y-%m-%d %H:%M:%S")
+    # logger settings
+    log_level = options.loglevel * 10
+    setup_general_log(options.log, log_level)
+    reader_log.setLevel(log_level)
 
     connector = partial(chat_connector, options.host, options.port, options.delay, options.retries, read_write_lines)
-    chat_handler = partial(client_reader, options.history, connector)
+    chat_handler = partial(read_chat, options.history, connector)
 
-    logging.info(f"Chat reader is starting with options: {options}")
+    reader_log.info(f"Chat reader is starting with options: {options}")
     run_client(chat_handler)
